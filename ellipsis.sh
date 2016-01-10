60a9edc0aca10eb20ef33703d18993d01a5858f0
G. Roggemans <g.roggemans@grog.be>
2016-01-10 13:45:00 +0100

    Added ellipsis.sh and implemented link hook

---
+++ ellipsis.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+##############################################################################
+# @file ellipsis.sh
+# @date January, 2016
+# @author G. Roggemans <g.roggemans@grog.be>
+# @copyright Copyright (c) GROG [https://grog.be] 2016, All Rights Reserved
+##############################################################################
+
+pkg.link() {
+    # Link package into ~/.scripts
+    fs.link_file $PKG_PATH
+}
+
+##############################################################################
+
