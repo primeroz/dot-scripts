97a3bb05c147ff1134a2fddc95bfa2fffdf01bff
Francesco Ciocchetti <primeroznl@gmail.com>
2016-04-15 21:44:18 +0100

    Test installing bin

--- ellipsis.sh
+++ ellipsis.sh
@@ -8,8 +8,29 @@
 
 pkg.link() {
     # Link package into ~/.scripts
-    fs.link_file "$PKG_PATH"
+    #fs.link_file "$PKG_PATH"
+    #fs.link_file "$PKG_PATH" "$ELLIPSIS_HOME/.config/i3"
+    exit 0
 }
 
 ##############################################################################
 
+pkg.install(){
+    mkdir -p $PKG_PATH/bin
+    cd $PKG_PATH/bin
+    wget -O - https://github.com/jtopjian/terminus/releases/download/v0.1.0/terminus.gz | tar xvf -
+}
+
+##############################################################################
+
+pkg.pull(){
+
+    # Update dot-i3 repo
+    #git.pull
+
+    # Update the config file
+    #$PKG_PATH/compile.sh
+    exit 0
+}
+
+##############################################################################
